bbb55a446f5a2f463902a62be6693c9cff4d1aa4
Arnett Rufino <arn3tt@gmail.com>
2019-04-18 11:32:54 -0300

    Initialize database with available vlan ids read from conf file

--- src/main/java/cloud/fogbow/vlanid/Main.java
+++ src/main/java/cloud/fogbow/vlanid/Main.java
@@ -1,11 +1,15 @@
 package cloud.fogbow.vlanid;
 
 import cloud.fogbow.common.exceptions.FatalErrorException;
+import cloud.fogbow.common.exceptions.UnexpectedException;
+import cloud.fogbow.vlanid.constants.ConfigurationPropertyDefaults;
 import cloud.fogbow.vlanid.constants.Messages;
 import cloud.fogbow.vlanid.core.ApplicationFacade;
+import cloud.fogbow.vlanid.core.PropertiesHolder;
 import cloud.fogbow.vlanid.core.VlanIdStateController;
 import cloud.fogbow.vlanid.core.datastore.DatabaseManager;
 import cloud.fogbow.vlanid.core.datastore.VlanIdStateRecoveryService;
+import cloud.fogbow.vlanid.core.model.VlanIdState;
 import cloud.fogbow.vlanid.core.xmpp.*;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +18,17 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Component
 public class Main implements ApplicationRunner {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static final Pattern RANGE_PATTERN = Pattern.compile("(\\d)+\\-(\\d)+");
 
     @Autowired
-    VlanIdStateRecoveryService recoveryService;
-
-    private static final Logger LOGGER = Logger.getLogger(Main.class);
+    private VlanIdStateRecoveryService recoveryService;
 
     private ApplicationFacade applicationFacade = ApplicationFacade.getInstance();
 
@@ -47,9 +54,30 @@ public class Main implements ApplicationRunner {
                     }
                 }
             }
-        } catch (FatalErrorException e) {
+            String vlanIdsRange = PropertiesHolder.getInstance().getProperty(ConfigurationPropertyDefaults.VLAN_IDS);
+            initializeVlanIdDatabase(vlanIdsRange);
+        } catch (FatalErrorException|UnexpectedException e) {
             LOGGER.fatal(e.getMessage(), e);
             tryExit();
+        }
+    }
+
+    private void initializeVlanIdDatabase(String vlanIdsRange) throws UnexpectedException {
+        Matcher matcher = RANGE_PATTERN.matcher(vlanIdsRange);
+        if (matcher.matches()) {
+            int start = Integer.parseInt(vlanIdsRange.split("\\-")[0]);
+            int end = Integer.parseInt(vlanIdsRange.split("\\-")[1]);
+
+            for (int i = start; i <= end; i++) {
+                try {
+                    recoveryService.put(new VlanIdState(i));
+                } catch (UnexpectedException e) {
+                    throw e;
+                }
+                // TODO DFNS see the exception that is thrown when the vlan id is already on the db
+            }
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 
